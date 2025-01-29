b25138584de2e516f34804946b67bad9b206d2b3
Zachary Pearce <zpearce92@gmail.com>
2025-01-29 19:27:56 +0000

    adding image to quiz container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,42 @@
 const question = document.getElementById("question");
 const options = Array.from(document.getElementsByClassName("answer-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        /*
+        question string
+        choice 1
+        choice 2
+        choice 3
+        choice 4 string
+        answer integar
+        */
+    },
+]
+
+//constants
+
+const correct_bonus = 1;
+const max_questions = 5;
+
+startQuiz = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    nextQuestion();
+};
+
+nextQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+}
+
+startQuiz();
